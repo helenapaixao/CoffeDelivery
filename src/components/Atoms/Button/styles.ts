6f523fec171381ca46241492c8d8569831cc8a90
helenapaixao <hp.helenapaixao@gmail.com>
2023-12-27 12:28:15 -0300

    [refactor] Improve layout in button component

--- src/components/Atoms/Button/styles.ts
+++ src/components/Atoms/Button/styles.ts
@@ -2,9 +2,6 @@ import styled from "styled-components";
 import { ButtonProps } from ".";
 
 export const Container = styled.button<ButtonProps>`
-  justify-content: center;
-  position: relative;
-  width: 100%;
   border-radius: 6px;
   height: 38px;
   width: ${({ size }) =>
@@ -13,19 +10,25 @@ export const Container = styled.button<ButtonProps>`
   outline: none;
   background-color: ${({ size }) =>
     size === "s" ? "#F1E9C9" : size === "m" ? "#EBE5F9" : "#DBAC2C"};
+  display: flex; 
+  align-items: center; 
 `;
 
 export const IconContainer = styled.div<{ size: string }>`
-  border: none;
-  outline: none;
+  svg {
+    width: ${({ size }) =>
+      size === "s" ? "20px" : size === "m" ? "20px" : "20px"};
+    height: ${({ size }) =>
+      size === "s" ? "20px" : size === "m" ? "20px" : "20px"};
+    color: ${({ size }) =>
+      size === "s" ? "#C47F17" : size === "m" ? "#8047F8" : "#FFFF"};
+  }
 `;
 
 export const Title = styled.h1<{ size: string; hasIcon: boolean }>`
-
-  margin-left: ${({ hasIcon }) => (hasIcon ? "27px" : "0px")};
+  margin-left: ${({ hasIcon }) => (hasIcon ? "10px" : "0")};
   color: ${({ size }) =>
     size === "s" ? "#4B2995" : size === "m" ? "#4B2995" : "#FFFF"};
-  display: flex;
   font-weight: ${({ size }) =>
     size === "s" ? "200" : size === "m" ? "400" : "700"};
   font-family: "Roboto", sans-serif;
--- src/components/Molecules/Header/index.tsx
+++ src/components/Molecules/Header/index.tsx
@@ -2,13 +2,13 @@ import Logo from "../../../assets/logo.svg";
 import { Image, Container } from "./styles";
 import { Button } from "../../Atoms/Button";
 import {BsCartFill} from 'react-icons/bs'
-
+import { FaLocationDot } from "react-icons/fa6";
 export const Header = () => {
   return (
     <Container>
       <Image src={Logo} />
-      <Button hasIcon={true} onPress={() => null} size="m" icon={BsCartFill } title="Porto Alegre, RS"/>
-      <Button hasIcon={true} onPress={() => null} size="s" icon={BsCartFill } />
+      <Button hasIcon={true} onPress={() => null} size="m" icon={<FaLocationDot/> } title="Porto Alegre, RS"/>
+      <Button hasIcon={true} onPress={() => null} size="s" icon={<BsCartFill/> } />
     </Container>
   );
 };
