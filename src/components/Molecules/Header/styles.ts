3a52a8d867f99571b8bd0a175f6bb9ba678450af
helenapaixao <hp.helenapaixao@gmail.com>
2023-12-31 13:19:55 -0300

    [refactor] Refactored component styling adjusted

--- src/components/Molecules/Header/styles.ts
+++ src/components/Molecules/Header/styles.ts
@@ -2,16 +2,12 @@ import  styled from 'styled-components';
 
 
 export const Container = styled.div`
-//display: flex;
+display: flex;
 width: 100%;
 height:104px ;
-justify-content:  space-between ;
-
-
 `;
 
 export const Image = styled.img`
-//margin-left: 160px ;
-//justify-content: center ;
-//align-items:  center;
+width: 85px;
+height:40px ;
 `;
