fcfceb2369f0662e6c21d42bcc578661b9d405d6
helenapaixao <hp.helenapaixao@gmail.com>
2023-12-31 13:23:24 -0300

    [chore] Update Vite configuration for React Project
    Added the vitejs/plugin-react plugin to enable React support in the Configured

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': path.resolve(new URL('.', import.meta.url).pathname, 'src/components'),
+      '@screens': path.resolve(new URL('.', import.meta.url).pathname, 'src/screens'),
+      '@assets': path.resolve(new URL('.', import.meta.url).pathname, 'src/assets'),
+      '@utils': path.resolve(new URL('.', import.meta.url).pathname, 'src/utils'),
+      '@services': path.resolve(new URL('.', import.meta.url).pathname, 'src/services'),
+      '@atoms': path.resolve(new URL('.', import.meta.url).pathname, 'src/components/Atoms'),
+      '@molecules': path.resolve(new URL('.', import.meta.url).pathname, 'src/components/Molecules'),
+      '@organisms': path.resolve(new URL('.', import.meta.url).pathname, 'src/components/Organisms'),
+      '@templates': path.resolve(new URL('.', import.meta.url).pathname, 'src/components/Templates'),
+      '@pages': path.resolve(new URL('.', import.meta.url).pathname, 'src/components/Pages'),
+    },
+  },
+};
