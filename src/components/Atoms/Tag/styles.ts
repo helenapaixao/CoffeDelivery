2223f15cd40af119899fd7373b75eec3eb45f61e
helenapaixao <hp.helenapaixao@gmail.com>
2024-02-19 15:18:39 -0300

    [fix] Add mixins in tags style

--- src/components/Atoms/Tag/styles.ts
+++ src/components/Atoms/Tag/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { mixins } from '../../../styles/mixins'
 export const Container = styled.div`
   background-color: ${(props) => props.theme.colors["yellow-light"]};
   border-radius: 100px;
@@ -7,15 +7,17 @@ export const Container = styled.div`
   height: 21px;
   align-items: center;
   justify-content: center;
+  text-transform: uppercase;
   display: flex;
 `;
 
 export const TextTag = styled.h1`
-  font-family: "Baloo 2", cursive;
   color: ${(props) => props.theme.colors["yellow-dark"]};
   font-weight: 700;
   font-size: 10px;
   text-align: center;
   justify-items: center;
   align-items: center;
+  gap: 4px;
+  ${mixins.fonts.tag}
 `;
