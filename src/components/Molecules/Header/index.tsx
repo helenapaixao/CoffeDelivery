ec91528fc8bdb79124e64535e62ffaccc69f3222
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-17 14:19:52 -0300

    [feat] Add geolocation functionality to header component to fetch and display

--- src/components/Molecules/Header/index.tsx
+++ src/components/Molecules/Header/index.tsx
@@ -1,20 +1,62 @@
+import React, { useEffect, useState } from "react";
 import Logo from "@assets/logo.svg";
-import { Image, Container } from "./styles";
+import { Image, Container, Aside } from "./styles";
 import { Button } from "../../Atoms/Button";
-import {BsCartFill} from 'react-icons/bs'
+import { BsCartFill } from "react-icons/bs";
 import { FaLocationDot } from "react-icons/fa6";
+import axios from "axios";
+
 export const Header = () => {
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        getUserCityAndState(latitude, longitude);
+      });
+    }
+  }, []);
+
+  const getUserCityAndState = async (latitude: number, longitude: number) => {
+    try {
+      const response = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?key=ec7758ac89324cbab7e4d86369d6f802&q=${latitude}0%2C+${longitude}&pretty=1&no_annotations=1
+        `
+      );
+      console.log(response.data);
+      const { city, state } = response.data.results[0].components;
+      setUserLocation(`${city}, ${state}`);
+    } catch (error) {
+      console.error("Error fetching user location:", error);
+    }
+  };
+
   return (
     <Container>
       <Image src={Logo} />
-      <div style={{
-        flexDirection: 'row',
-        marginLeft: '12px' 
-      }}>
-      <Button hasIcon={true} onPress={() => null} size="m" icon={<FaLocationDot/> } title="Porto Alegre, RS"/>
+      <div
+        style={{
+          flexDirection: "row",
+          marginLeft: "12px",
+        }}
+      ></div>
+      <Aside>
+        <Button
+          hasIcon={true}
+          onPress={() => null}
+          size="m"
+          icon={<FaLocationDot />}
+          title={userLocation ? userLocation : "Obtendo localização..."}
+        />
 
-      </div>
-      <Button hasIcon={true} onPress={() => null} size="s" icon={<BsCartFill/> } />
+        <Button
+          hasIcon={true}
+          onPress={() => null}
+          size="s"
+          icon={<BsCartFill />}
+        />
+      </Aside>
     </Container>
   );
 };
