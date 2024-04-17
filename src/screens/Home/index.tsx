41204acf255b62dfd5c14071fcce8e032079478e
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-17 14:18:12 -0300

    [refactor] Updated home component to display a list of coffees from data.json

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,23 @@
 import { Header } from "../../components/Molecules/Header";
-import Intro from "../../assets/Imagem.svg";
-import { Content } from "./styles";
+import { CoffeeList, Content } from "./styles";
 import { Card } from "../../components/Atoms/Card";
-import Coffee from "../../assets/Coffee.svg";
+import { coffees } from "../../../data.json";
+import { Banner } from "../../components/Atoms/Banner";
 
 export const Home = () => {
   return (
     <>
       <Header />
+      <Banner/>
       <Content>
-        <img src={Intro} />
-        <Card
-          tagTexts={["TRADICIONAL", "COM LEITE", "GELADO"]}
-          src={Coffee}
-          title="Expresso Americano"
-          content="Expresso diluído, menos intenso que o tradicional"
-          price="9,90"
-        />
+        <CoffeeList>
+        <div>
+          {coffees.map((coffee) => (
+            <Card key={coffee.id} coffee={coffee} />
+          ))}
+        </div>
+        </CoffeeList>
+        
       </Content>
     </>
   );
