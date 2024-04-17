a64b060d8fb342329017239bc62fa3964ce5a0c4
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-17 14:52:44 -0300

    [refactor] Updated header component to use custom hooks

--- src/components/Molecules/Header/index.tsx
+++ src/components/Molecules/Header/index.tsx
@@ -4,33 +4,10 @@ import { Image, Container, Aside } from "./styles";
 import { Button } from "../../Atoms/Button";
 import { BsCartFill } from "react-icons/bs";
 import { FaLocationDot } from "react-icons/fa6";
-import axios from "axios";
+import useUserLocation from "../../../hooks/useUserLocation";
 
 export const Header = () => {
-  const [userLocation, setUserLocation] = useState(null);
-
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        getUserCityAndState(latitude, longitude);
-      });
-    }
-  }, []);
-
-  const getUserCityAndState = async (latitude: number, longitude: number) => {
-    try {
-      const response = await axios.get(
-        `https://api.opencagedata.com/geocode/v1/json?key=ec7758ac89324cbab7e4d86369d6f802&q=${latitude}0%2C+${longitude}&pretty=1&no_annotations=1
-        `
-      );
-      console.log(response.data);
-      const { city, state } = response.data.results[0].components;
-      setUserLocation(`${city}, ${state}`);
-    } catch (error) {
-      console.error("Error fetching user location:", error);
-    }
-  };
+  const userLocation = useUserLocation();
 
   return (
     <Container>
