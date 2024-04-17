1fe4358065046dd8fc052569b06e956cd6221bf9
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-17 14:15:54 -0300

    [refactor] Updated styles in coffeelist component

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mixins } from "../../styles/mixins";
 
 export const Container = styled.div`
   display: flex;
@@ -7,3 +8,26 @@ export const Container = styled.div`
 export const Content = styled.div`
   grid-template-columns: 0px;
 `;
+
+
+export const CoffeeList = styled.section`
+  max-width: 1160px;
+  padding: 32px 20px 150px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 54px;
+
+  > h2 {
+    ${mixins.fonts.titleL}
+    color: ${({ theme }) => theme.colors['base-subtitle']}
+  }
+
+  > div {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-row-gap: 40px;
+    grid-column-gap: 32px;
+  }
+`
