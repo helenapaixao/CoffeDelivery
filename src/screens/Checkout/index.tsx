5c66fc43a1643b27fcef43a15362ff42cb29e341
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-22 22:39:24 -0300

    [chore] Add style in div for test

--- src/screens/Checkout/index.tsx
+++ src/screens/Checkout/index.tsx
@@ -1,14 +1,18 @@
 
-import { Input } from '../../components/Atoms/Input';
-
+import {Form} from '../../components/Molecules/Form'
 
 
 
 export const Checkout = () => {
   return (
-    <div>
-      <h1>Checkout</h1>
-      <Input type="text" value="" onChange={() => {}} placeholder="Nome" />
+    <div style={
+      {
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh'
+      }
+    }>
+      <Form />
     </div>
   )
 }
