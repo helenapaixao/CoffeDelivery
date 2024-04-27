dd75ca8d354f0abf7fe001192a1e0ec236f0b559
helenapaixao <hp.helenapaixao@gmail.com>
2024-04-27 14:41:51 -0300

    [feat] Add isSelected  and size props to buttonCart component

--- src/components/Molecules/ButtonCart/index.tsx
+++ src/components/Molecules/ButtonCart/index.tsx
@@ -1,16 +1,22 @@
-import { Container, IconContainer } from "./styles";
+import { ButtonHTMLAttributes } from "react";
+import { Container, IconContainer, Title } from "./styles";
 
-export type ButtonProps = {
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   icon?: React.ReactNode;
   onPress: () => void;
   title?: string;
   hasIcon?: boolean;
+  isSelected: boolean;
+  size?: number;
 };
 
-export const ButtonCart = ({ icon, onPress }: ButtonProps) => {
+export const ButtonCart = ({ icon, onPress, title, size, isSelected }: ButtonProps) => {
   return (
-    <Container onPress={onPress}>
+    <Container isSelected={isSelected} onPress={onPress}>
       {icon && <IconContainer>{icon}</IconContainer>}
+      <Title hasIcon={!!icon} size={size}>
+        {title}
+      </Title>
     </Container>
   );
 };
