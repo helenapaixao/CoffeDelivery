56b787c529331054298d4bdb58f49398ad78678b
helenapaixao <hp.helenapaixao@gmail.com>
2025-11-05 10:52:08 -0300

    chore: Update dependencies in yarn.lock and remove Header component from App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { RouterProvider } from "react-router-dom";
 import { defaultTheme } from "./styles/themes/default";
-import {Header} from "../src/components/Molecules/Header";
 import router from "./routes/router";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Header />
       <RouterProvider router={router}/>
     </ThemeProvider>
   );
